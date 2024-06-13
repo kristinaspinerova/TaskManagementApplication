b5051f86816b1adcd3f2c632baf0fc0ca0697152
Kristina Spinerova <kristina.spinerova@gmail.com>
2024-06-13 23:34:59 +0200

    Some little changes according to comments

--- frontend/src/components/Task/UpdateTaskStatus.js
+++ frontend/src/components/Task/UpdateTaskStatus.js
@@ -2,16 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios"; 
 import { useParams, useNavigate, json} from "react-router-dom";
   
-function TaskList() { 
-    const [tasklist, setTaskList] = useState([]); 
+function UpdateTaskStatus() { 
     const navigate = useNavigate();
     const { id } = useParams();
   
     useEffect(() => { 
         const loadPost = async () => { 
-  
             let newStatus = prompt("New status?");
-            let copy = JSON.parse(JSON.stringify(tasklist))
             const response = await axios.put(`http://localhost/task/${id}/status/${newStatus}`);
             navigate("/")
         }; 
@@ -26,4 +23,4 @@ function TaskList() {
     ); 
 } 
   
-export default TaskList; 
+export default UpdateTaskStatus; 
